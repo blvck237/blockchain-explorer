26021a8d40e40888cb9bdc437d1548ad79fd8ac8
Jordan Fokoua <jfokoua@gmail.com>
2019-06-02 22:09:03 +0200

    :sparkles: Set up router view

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav> </Nav>
+    <transition name="fade">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Home from './pages/Home.vue'
+import Nav from './components/Nav.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Nav
   }
 }
 </script>
