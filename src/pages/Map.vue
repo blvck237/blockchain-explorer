724072894320fa52e0b4ea95632da0720dc9c511
Jordan Fokoua <jfokoua@gmail.com>
2019-06-02 23:10:58 +0200

    :sparkles: Implement map

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,13 +1,54 @@
 <template>
-    
+  <div class="map">
+    <tree class="tree"
+      :data="tree"
+      node-text="name"
+      type="tree"
+      layoutType="euclidean"
+      :duration="duration"
+      :radius="radius"
+      :marginx="margin"
+      :marginy="margin"
+      zoomable="true"
+    ></tree>
+  </div>
 </template>
 
 <script>
-export default {
-    name: "Map"
-}
+  import { tree } from "vued3tree";
+  export default {
+    name: "Map",
+    components: {
+      tree
+    },
+    data() {
+      return {
+        duration: 100,
+        radius: 5,
+        margin: 30,
+        tree: {
+          name: "father",
+          children: [
+            {
+              name: "son1",
+              children: [{ name: "grandson" }, { name: "grandson2" }]
+            },
+            {
+              name: "son2",
+              children: [{ name: "grandson3" }, { name: "grandson4" }]
+            }
+          ]
+        }
+      };
+    }
+  };
 </script>
 
-<style>
-
+<style scoped>
+    .map{
+        height: 55em;
+    }
+    text{
+        font-size: 20px;
+    }
 </style>
