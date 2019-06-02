4de3a384449a1f85deb46b8afbc8910471476507
Jordan Fokoua <jfokoua@gmail.com>
2019-06-02 22:08:22 +0200

    :sparkles: Create components  & pages

---
+++ src/components/Nav.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <vs-navbar v-model="activeItem" class="nabarx">
+      <div slot="title">
+        <vs-navbar-title>
+         <img src="../assets/logo.png" alt="logo" width="50">
+        </vs-navbar-title>
+      </div>
+      <vs-navbar-item class="menu-btn" index="0">
+        <router-link tag="a" to="/home">Home</router-link>
+      </vs-navbar-item>
+      <vs-navbar-item class="menu-btn" index="1">
+        <router-link tag="a" to="/transactions">Transactions</router-link>
+      </vs-navbar-item>
+      <vs-navbar-item class="menu-btn" index="2">
+        <router-link tag="a" to="/blocks">Blocks</router-link>
+      </vs-navbar-item>
+      <vs-navbar-item class="menu-btn" index="3">
+        <router-link tag="a" to="/map">Livemap</router-link>
+      </vs-navbar-item>
+      </vs-navbar-item>
+      <vs-navbar-item class="menu-btn" index="4">
+        <router-link tag="a" to="/login">My wallet</router-link>
+      </vs-navbar-item>
+    </vs-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+
+}
+</script>
+
+
+<style>
+.nabarx{
+    padding: 1em;
+}
+.menu-btn:hover a{
+    color: red
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div>
+      <h1>Welcome {{ walletInfo.address }} to your blockchain explorer</h1>
+      <!-- <vs-button v-on:click="login" color="primary" type="filled"
+        >Primary</vs-button
+      > -->
+    </div>
+  </div>
+</template>
+
+<script>
+  import { request } from "../../utils/api";
+  export default {
+    name: "Home",
+    data() {
+      return {
+        walletInfo: {
+          address: "foo-add",
+          balance: 9000
+        }
+      };
+    },
+    beforeMount() {
+      this.getWalletInfo();
+    },
+    methods: {
+      login() {
+        request("supcoin/blocks")
+          .then(res => {
+            console.log("Log: login -> res", res);
+          })
+          .catch(err => {
+            console.log("Log: login -> err", err);
+          });
+      },
+      getWalletInfo() {
+        request("supcoin/wallet-info")
+          .then(res => {
+            console.log("Log: getWalletInfo -> res", res);
+          })
+          .catch(err => {
+            console.log("Log: getWalletInfo -> err", err);
+          });
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  h1{
+    margin: 1.5em;
+  }
+</style>
