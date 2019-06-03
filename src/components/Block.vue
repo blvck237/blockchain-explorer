76c4f8149f773d09dfa9083aab7ad6cba1b8961d
Jordan Fokoua <jfokoua@gmail.com>
2019-06-03 14:53:20 +0200

    :sparkles: Block component

---
+++ src/components/Block.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <vs-list>
+      <vs-list-header :title="id"></vs-list-header>
+      <vs-list-item
+        class="vs-list-item"
+        title="Hash"
+        :subtitle="hash"
+      ></vs-list-item>
+      <vs-list-item title="Date" :subtitle="timestamp"></vs-list-item>
+      <vs-list-item title="Transactions" :subtitle="data"></vs-list-item>
+    </vs-list>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Block",
+    props: {
+      id: Number,
+      hash: Number,
+      data: Array,
+      timestamp: Number
+    }
+  };
+</script>
+
+<style>
+  vs-list-item {
+    text-align: left;
+    color: blue;
+  }
+</style>
