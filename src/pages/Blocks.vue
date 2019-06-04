8423121e9d463af9220d6a2e2af9d736e82389ab
Jordan Fokoua <jfokoua@gmail.com>
2019-06-05 01:10:24 +0200

    :sparkles: Implement blocks

--- src/pages/Blocks.vue
+++ src/pages/Blocks.vue
@@ -1,14 +1,62 @@
 <template>
-    
+  <div class="blocks">
+    <h1>Blocks</h1>
+    <div class="" v-for="(block, index) in blocks" v-bind:key="block.hash">
+      <Block
+        class="block"
+        :id="index+1"
+        :hash="block.hash"
+        :timestamp="convertTime(block.timestamp)"
+        :data="block.data.length"
+      ></Block>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    name: "Blocks"
-}
+  //utils
+  import { request } from "../../utils";
+  import timeConverter from "../../utils/timeconverter";
+  // Component imports
+  import Block from "../components/Block";
+
+  export default {
+    name: "Blocks",
+    components: {
+      Block
+    },
+    data() {
+      return {
+        blocks: []
+      };
+    },
+    beforeMount() {
+      this.getBlocks();
+    },
+    methods: {
+      getBlocks() {
+        request("supcoin/blocks")
+          .then(res => {
+            this.blocks = res.data;
+            console.log("Log: getBlocks -> res", res);
+            for (let i = 0; i < res.data.length; i++) {
+              this.convertTime(res.data[i].timestamp);
+            }
+          })
+          .catch(err => {
+            console.log("Log: getBlocks -> err", err);
+          });
+      },
+      convertTime(timestamp) {
+        return timeConverter(timestamp);
+      }
+    }
+  };
 </script>
 
 <style>
-
+  .block {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
 </style>
-
