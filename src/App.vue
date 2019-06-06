566da5090a11d0218905a3725b174960ebf5c95f
Jordan Fokoua <jfokoua@gmail.com>
2019-06-06 19:46:31 +0200

    Improve router views

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
   <div id="app">
-    <Nav> </Nav>
     <transition name="fade">
-      <router-view/>
+      <keep-alive>
+        <router-view ></router-view>
+      </keep-alive>
     </transition>
   </div>
 </template>
 
 <script>
-// import Home from './pages/Home.vue'
-import Nav from './components/Nav.vue'
-export default {
-  name: 'app',
-  components: {
-    Nav
-  }
-}
+  // import Home from './pages/Home.vue'
+  import Nav from "./components/Nav.vue";
+  import "material-icons/iconfont/material-icons.css";
+  export default {
+    name: "app",
+    components: {
+      Nav
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .nav {
+    padding-bottom: 2.5em;
+  }
+  .router-link-active {
+    border-bottom: 2px solid #006084;
+    color: #006084;
+  }
+  .router-link-active a {
+    color: #006084;
+  }
 </style>
